7e9d30493c4b5f393eea9fbc94b283add2b3983b
pedro-bolinha <pedro-neto.pn@academico.ifpb.edu.br>
2025-05-07 10:38:19 -0300

    Create empresa.js

---
+++ empresa.js
@@ -0,0 +1,20 @@
+export const empresas = [
+    {
+      "name": "Empresa Alpha",
+      "descricao": "Especializada em soluções de tecnologia.",
+      "img": "alpha.jpg",
+      "preco": "R$ 200"
+    },
+    {
+      "name": "Empresa Beta",
+      "descricao": "Consultoria empresarial e marketing.",
+      "img": "beta.jpg",
+      "preco": "R$ 150"
+    },
+    {
+      "name": "Empresa Gama",
+      "descricao": "Serviços de design e branding.",
+      "img": "gama.jpg",
+      "preco": "R$ 180"
+    }
+  ]
