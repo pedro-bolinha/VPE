f6584d466ec7aa59d900fa205065257498d0b2c8
pedro-bolinha <pedro-neto.pn@academico.ifpb.edu.br>
2025-05-17 12:05:24 -0300

    Update and rename lista_de_empresa.js to empresa.js

---
+++ empresa.js
@@ -0,0 +1,102 @@
+export const empresas = [
+  {
+    name: "FAMPEPAR",
+    descricao: "Venda de bala e pirulito.",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThdm500SGyX7t-_1Ux9F7F68r2STyLh1Pq-g&s",
+    preco: 200,
+    dadosFinanceiros: [
+      { mes: "Janeiro", valor: 25000 },
+      { mes: "Fevereiro", valor: 30000 },
+      { mes: "Março", valor: 18000 },
+      { mes: "Abril", valor: 26000 },
+      { mes: "Maio", valor: 30868 },
+      { mes: "Junho", valor: 42000 },
+      { mes: "Julho", valor: 27920 },
+      { mes: "Agosto", valor: 18100 },
+      { mes: "Setembro", valor: 17000 },
+      { mes: "Outubro", valor: 29000 },
+      { mes: "Novembro", valor: 50000 },
+      { mes: "Dezembro", valor: 10000 }
+    ]
+  },
+  {
+    name: "Bolo do Dia",
+    descricao: "Loja de bolos e tortas.",
+    img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Freceitatodahora.com.br%2Fa-melhor-versao-de-bolo-mole-que-existe-parece-pudim-de-tao-cremosao%2F&psig=AOvVaw3ZT05Ti8Kw0yGFwEwYVhF7&ust=1747576562683000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqfwoTCNCehJfUqo0DFQAAAAAdAAAAABAK",
+    preco: 150,
+    dadosFinanceiros: [
+      { mes: "Janeiro", valor: 12000 },
+      { mes: "Fevereiro", valor: 18000 },
+      { mes: "Março", valor: 15000 },
+      { mes: "Abril", valor: 16000 },
+      { mes: "Maio", valor: 18500 },
+      { mes: "Junho", valor: 21000 },
+      { mes: "Julho", valor: 18000 },
+      { mes: "Agosto", valor: 22000 },
+      { mes: "Setembro", valor: 20000 },
+      { mes: "Outubro", valor: 19000 },
+      { mes: "Novembro", valor: 25000 },
+      { mes: "Dezembro", valor: 24000 }
+    ]
+  },
+  {
+    name: "Salgado Dahora",
+    descricao: "Loja de salgados.",
+    img: "https://www.google.com/imgres?q=salgado&imgurl=https%3A%2F%2Fwww.diariozonanorte.com.br%2Fwp-content%2Fuploads%2F2020%2F07%2Fcoxinha.jpg&imgrefurl=https%3A%2F%2Fwww.diariozonanorte.com.br%2Fo-salgado-queridinho-do-brasil-tem-origem-nobre-e-um-dia-todinho-seu-18-de-maio%2F&docid=ev9lIkxbu5_pGM&tbnid=ZmyfrtUyM2celM&vet=12ahUKEwjd4_6Z06qNAxVcKrkGHWKGIRcQM3oECEwQAA..i&w=600&h=374&hcb=2&ved=2ahUKEwjd4_6Z06qNAxVcKrkGHWKGIRcQM3oECEwQAA",
+    preco: 180,
+    dadosFinanceiros: [
+      { mes: "Janeiro", valor: 15000 },
+      { mes: "Fevereiro", valor: 22000 },
+      { mes: "Março", valor: 17000 },
+      { mes: "Abril", valor: 19000 },
+      { mes: "Maio", valor: 21000 },
+      { mes: "Junho", valor: 24000 },
+      { mes: "Julho", valor: 23000 },
+      { mes: "Agosto", valor: 25000 },
+      { mes: "Setembro", valor: 26000 },
+      { mes: "Outubro", valor: 23000 },
+      { mes: "Novembro", valor: 28000 },
+      { mes: "Dezembro", valor: 27000 }
+    ]
+  },
+  {
+    name: "Basket Pro",
+    descricao: "Loja em equipamentos para basquete.",
+    img: "https://images.unsplash.com/photo-1546519638-68e109498ffc?auto=format&fit=crop&w=600&q=60",
+    preco: 320,
+    dadosFinanceiros: [
+      { mes: "Janeiro", valor: 45000 },
+      { mes: "Fevereiro", valor: 50000 },
+      { mes: "Março", valor: 48000 },
+      { mes: "Abril", valor: 55000 },
+      { mes: "Maio", valor: 60000 },
+      { mes: "Junho", valor: 70000 },
+      { mes: "Julho", valor: 67000 },
+      { mes: "Agosto", valor: 72000 },
+      { mes: "Setembro", valor: 71000 },
+      { mes: "Outubro", valor: 75000 },
+      { mes: "Novembro", valor: 80000 },
+      { mes: "Dezembro", valor: 85000 }
+    ]
+  },
+  {
+    name: "Refrescos Brasil",
+    descricao: "Distribuidora de bebidas e refrigerantes.",
+    img: "https://images.unsplash.com/photo-1598511727478-950dbcd31b26?auto=format&fit=crop&w=600&q=60",
+    preco: 210,
+    dadosFinanceiros: [
+      { mes: "Janeiro", valor: 30000 },
+      { mes: "Fevereiro", valor: 35000 },
+      { mes: "Março", valor: 34000 },
+      { mes: "Abril", valor: 36000 },
+      { mes: "Maio", valor: 40000 },
+      { mes: "Junho", valor: 42000 },
+      { mes: "Julho", valor: 45000 },
+      { mes: "Agosto", valor: 46000 },
+      { mes: "Setembro", valor: 43000 },
+      { mes: "Outubro", valor: 48000 },
+      { mes: "Novembro", valor: 50000 },
+      { mes: "Dezembro", valor: 55000 }
+    ]
+  }
+];
